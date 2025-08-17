03906d71293b428a890279ef559061b3f863a4e5
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-08-17 17:26:25 +0530

    Created config class for managing beans.

---
+++ src/main/java/in/vyashivam/creativerse/configuration/AiConfiguration.java
@@ -0,0 +1,33 @@
+package in.vyashivam.creativerse.configuration;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class AiConfiguration {
+
+    //Implementation of ChatClient bean through OpenAiChatModel for this project
+    @Bean("chatClient")
+    @Primary
+    public ChatClient createChatClientBean(OpenAiChatModel openAiChatModel) {
+        return ChatClient.create(openAiChatModel);
+    }
+
+    @Bean("chatMemory")
+    public ChatMemory createChatMemoryBean() {
+        return MessageWindowChatMemory.builder().build();
+    }
+
+    //Implementation of ChatClient bean for chat recall via Advisors
+    @Bean("chatClientWithMemory")
+    public ChatClient createChatClientWithMemory(ChatClient.Builder builder, @Qualifier("chatMemory") ChatMemory chatMemory) {
+        return builder.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build()).build();
+    }
+}
