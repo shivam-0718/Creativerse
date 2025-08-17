89103889489844cfed36d1cfcf569a4e66581ae0
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-08-17 16:47:18 +0530

    Creating the project with dependencies and configurations.

---
+++ src/main/java/in/vyashivam/creativerse/CreativerseApplication.java
@@ -0,0 +1,13 @@
+package in.vyashivam.creativerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreativerseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/vyashivam/creativerse/CreativerseApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vyashivam.creativerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
